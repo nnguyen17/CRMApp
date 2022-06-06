6c49548fe12c4216d3c840ae4cb242db30bf37d1
Madhusudan Sharma <sharmamadhusudan@hotmail.com>
2022-06-06 17:21:56 -0500

    Add project files.

---
+++ Antra.CRMApp.WebMVC/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Antra.CRMApp.WebMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Product/Index";
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            ViewData["Title"] = "Product/Details";
+            return View("explain");
+        }
+    }
+}
